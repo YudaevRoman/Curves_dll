6fce5623dfe7c030ee87ef76df213bad41fd76b5
Leere Ewigkeit <ewileere@gmail.com>
2023-09-16 10:41:47 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Leere/Desktop/Curves_dll
+# Build directory: C:/Users/Leere/Desktop/Curves_dll/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
